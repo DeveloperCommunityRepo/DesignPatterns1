edf517d9f51bbd1aed69016395670b1917af21a2
Taner Saydam <taner_and_you@hotmail.com>
2025-10-21 20:12:00 +0300

    Add project files.

---
+++ 01_CretionalPatterns/01_SingletonPattern/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Singleton Pattern...");
+
+ServiceCollection services = new();
+services.AddSingleton<Logger2>();
+
+var srv = services.BuildServiceProvider();
+var logger = srv.GetRequiredService<Logger2>();
+logger.Log("Hello world");
+
+var logger2 = srv.GetRequiredService<Logger2>();
+logger.Log("Hello world");
+
+Console.ReadLine();
+
+class Logger2
+{
+    public Logger2()
+    {
+
+    }
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+#region Classic Singleton Pattern
+//Logger.Instance.Log("Hello world1");
+//Logger.Instance.Log("Hello world2");
+//Logger.Instance.Log("Hello world3");
+
+//LoggerStatic.Log("Hello world");
+static class LoggerStatic
+{
+    public static void Log(string message)
+    {
+        Logger.Instance.Log(message);
+    }
+}
+
+class Logger
+{
+    private static Logger? _instance;
+    private static readonly object _lock = new object();
+    public static Logger Instance
+    {
+        get
+        {
+            lock (_lock)
+            {
+                if (_instance is null)
+                {
+                    _instance = new Logger();
+                }
+            }
+            return _instance;
+        }
+    }
+    private Logger()
+    {
+
+    }
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+#endregion
---
+++ 01_CretionalPatterns/02_FactoryPattern/Program.cs
@@ -0,0 +1,76 @@
+﻿Console.WriteLine("Factory Pattern...");
+
+var notification = NotificationFactory.CreateNotification(NotificationTypeEnum.Sms);
+notification.Send("hello world");
+
+var notification2 = NotificationFactory.CreateNotification(NotificationTypeEnum.Email);
+notification2.Send("hello world");
+
+var notification3 = NotificationFactory.CreateNotification(NotificationTypeEnum.Whatsapp);
+notification3.Send("hello world");
+
+Console.ReadLine();
+
+interface INotification
+{
+    void Send(string msg);
+}
+class SmsNotification : INotification
+{
+    public void Send(string msg)
+    {
+        Console.WriteLine("I send sms: {0}", msg);
+    }
+}
+
+class EmailNotification : INotification
+{
+    public void Send(string msg)
+    {
+        Console.WriteLine("I send email: {0}", msg);
+    }
+}
+
+class CloudeNotification : INotification
+{
+    public void Send(string msg)
+    {
+        Console.WriteLine("I send clode message: {0}", msg);
+    }
+}
+
+class WhatpsappNotification : INotification
+{
+    public void Send(string msg)
+    {
+        Console.WriteLine("I send whatpsapp message: {0}", msg);
+    }
+}
+
+class NotificationFactory
+{
+    public static INotification CreateNotification(NotificationTypeEnum type)
+    {
+        switch (type)
+        {
+            case NotificationTypeEnum.Sms:
+                return new SmsNotification();
+            case NotificationTypeEnum.Email:
+                return new EmailNotification();
+            case NotificationTypeEnum.Whatsapp:
+                return new WhatpsappNotification();
+            case NotificationTypeEnum.Cloude:
+                return new CloudeNotification();
+            default:
+                throw new ArgumentException("Invalid notification type");
+        }
+    }
+}
+
+enum NotificationTypeEnum
+{
+    Sms,
+    Email,
+    Whatsapp,
+    Cloude
+}
